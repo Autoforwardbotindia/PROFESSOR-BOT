977d284233be9df8ddbb29e913356e4fdd2f6222
Autoforwardbotindia <138660358+Autoforwardbotindia@users.noreply.github.com>
2023-07-07 22:21:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,9 +98,17 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
+        m=await message.reply_sticker("")
+        await asyncio.sleep(1)
+        await m.delete()
+        await message.reply_chat_action(enums.ChatAction.TYPING)
         m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
         await asyncio.sleep(1)
         await m.delete()
+       await message.reply_chat_action(enums.ChatAction.TYPING)
+        m=await message.reply_sticker("CAACAgUAAxkBAAEJoAlkqEIGunZ2ondh9tSQnxxAznHnUwACsQQAAlyNAVe6mxYkaWScui8E")
+        await asyncio.sleep(1)
+        await m.delete() 
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=START_MESSAGE.format(user=message.from_user.mention, bot=temp.B_LINK),
